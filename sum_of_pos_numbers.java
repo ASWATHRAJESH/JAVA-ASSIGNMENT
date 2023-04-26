3e449fce63e513c258a5b1a489e4119df810d8b2
ASWATHRAJESH <126188594+ASWATHRAJESH@users.noreply.github.com>
2023-04-26 17:39:31 +0530

    DAY 2 ASSIGNMENT COMPLETED

---
+++ sum_of_pos_numbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class sum_of_pos_numbers
+{
+    public static void main(String args[])
+    {
+        int n , sum = 0;
+        
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Enter series of numbers : ");
+
+        while(i>0)
+        {
+            n = s.nextInt();
+            
+            if(n>0)
+            {
+                sum = sum + n;
+            }
+            else
+            {
+                break;
+            }
+        }
+         
+        System.out.println("Sum of positive numbers entered in the series : " + sum);     
+    }
+}
